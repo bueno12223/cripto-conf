09d9fc7862ed9c1cf6bc21eb258b7178d87b668d
Jesus <berriojesus122@gmail.com>
2021-02-19 20:00:19 -0500

    7. home lista y responsive añadido

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,16 +11,19 @@ export default class Home extends Component {
     return (
       <React.Fragment>
         <Navbar></Navbar>
+        
         <div className="Home">
           <img className="home__background" src={cripto} alt=""/>
           <div className="home__info">
-          <img className="home__infoBatata" src={batataLogo} alt=""/>
-            
+          
             <div className="home_conf">
+            <img className="home__confBatata" src={batataLogo} alt=""/>
             <h1 className="home__confTitle home__titleOne">CRIPTO<br/></h1>
             <h1 className="home__confTitle home__titleTwo">CONF</h1>
+            <h2 className="home__confDescription">Sistema de registro</h2>
+          <button className="home__confButton">Registrarme</button>
           </div>
-          <button></button>
+          
           </div>
           
             
